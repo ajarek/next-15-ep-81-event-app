63a8a32567e3850b1803f3680e6b85e6dcefec2b
Ajarek <ajarek@poczta.onet.pl>
2025-03-17 00:21:38 +0100

    added SelectQuantity

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -3,13 +3,14 @@ import dataEvents from '@/public/db.json'
 import Image from 'next/image'
 import { CalendarDays, Dot, MapPin } from 'lucide-react'
 import CountdownTimer from '@/components/CountdownTimer'
-const EventId = async ({
-  searchParams,
-}: {
-  searchParams: Promise<{ id: string }>
-}) => {
-  const { id } = await searchParams
+import SelectSeats from '@/components/SelectSeats'
+import SelectQuantity from '@/components/SelectQuantity'
+
+const EventId = async ({searchParams}: {searchParams: Promise<{ id: string, seat_price: string, quantity: string }>}) => {
+
+  const { id, seat_price, quantity   } = await searchParams
   const event = dataEvents.find((event) => event.id === id)
+
   return (
     <div className='min-h-screen grid grid-cols-2 max-lg:grid-cols-1 gap-4 pt-16 place-items-center p-4'>
       <div className='relative'>
@@ -24,7 +25,7 @@ const EventId = async ({
       </div>
       <div className='w-full flex flex-col justify-center items-start gap-8'>
         <h1 className='text-2xl font-bold'>{event?.title || ''}</h1>
-        <div className='w-full flex items-center justify-between pr-8'>
+        <div className='w-full flex items-center gap-8'>
           <p className='flex  items-center gap-2 '>
             <CalendarDays color={'#ea580c'} />
             {event?.date || ''}
@@ -36,7 +37,19 @@ const EventId = async ({
             {event?.location || ''}
           </p>
         </div>
-        <CountdownTimer date={event?.date || ''} />
+        <CountdownTimer date={event?.date || ''} /> 
+        <SelectSeats query='seat_price' array={event?.seats || []}/>
+        <div className='w-full flex items-center gap-8'>
+
+        <SelectQuantity query='quantity'/>
+        {seat_price?
+        <div className='flex items-center py-2 px-6 rounded-2xl gap-2 bg-primary'>
+          {quantity} x ticket(s) - ${Number(seat_price?.replace(/\D/g, ''))*Number(quantity)}
+          </div>
+          :null
+          
+          }
+        </div>
       </div>
     </div>
   )
---
+++ components/SelectSeats.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { Armchair } from 'lucide-react'
+interface SearchProps {
+  query: string
+  array: { seat: string; price: number }[]
+}
+
+const SelectSeats = ({ query, array }: SearchProps) => {
+  const searchParams = useSearchParams()
+    const { replace } = useRouter()
+    const pathname = usePathname()
+
+    const handleSearch = (term: string) => {
+      const params = new URLSearchParams(searchParams)
+  
+      if (term) {
+        params.set(`${query}`, term)
+      } else {
+        params.delete(`${query}`)
+      }
+      try {
+        replace(`${pathname}?${params.toString()}`)
+      } catch (error) {
+        console.error('Failed to replace URL parameters:', error)
+      }
+    }
+  
+  
+
+  return (
+    <Select   onValueChange={(value) => handleSearch(value)}
+    defaultValue={searchParams.get(query)?.toString()}>
+          <SelectTrigger id={'event'} className='w-[300px] bg-background border border-primary py-6 text-lg'>
+            <SelectValue placeholder='Select Seat' />
+          </SelectTrigger>
+          <SelectContent>
+            {array.map((item, index) => (
+
+            <SelectItem key={index} value={`${item.price.toString()} ${item.seat}`}>
+              <div className=' flex  items-center gap-24' >
+                <span className='capitalize flex items-center gap-2 '><Armchair color={'#ea580c'} /> {item.seat}</span>
+                <span>$ {item.price}</span>
+              </div>
+            </SelectItem>
+            )
+            )}
+           
+          </SelectContent>
+        </Select>
+  )
+}
+
+export default SelectSeats
